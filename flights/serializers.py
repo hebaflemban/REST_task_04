7494ac49f4b24b069d70a2ad6d304c7eb0c9fc79
Heba <36724067+heba@users.noreply.github.com>
2020-09-15 11:25:36 +0300

    adds a user registration api view

--- flights/serializers.py
+++ flights/serializers.py
@@ -1,7 +1,25 @@
 from rest_framework import serializers
-
+from django.contrib.auth.models import User
 from .models import Flight, Booking
 
+class RegisterUserSerializer(serializers.ModelSerializer):
+	password = serializers.CharField(write_only = True)
+	class Meta:
+		model = User
+		fields = ['first_name', 'last_name', 'username', 'password' ]
+		extra_kwargs = {'first_name': {'required': True},
+						'last_name': {'required': True}}
+
+	def create (self, validated_data):
+		first_name = validated_data['first_name']
+		last_name = validated_data['last_name']
+		username = validated_data['username']
+		password = validated_data['password']
+		new_user = User(first_name=first_name, last_name=last_name, username = username)
+		new_user.set_password(password)
+		new_user.save()
+
+		return validated_data
 
 class FlightSerializer(serializers.ModelSerializer):
 	class Meta:
@@ -25,4 +43,3 @@ class UpdateBookingSerializer(serializers.ModelSerializer):
 	class Meta:
 		model = Booking
 		fields = ['date', 'passengers']
-
--- flights/views.py
+++ flights/views.py
@@ -2,8 +2,10 @@ from rest_framework.generics import ListAPIView, RetrieveAPIView, RetrieveUpdate
 from datetime import datetime
 
 from .models import Flight, Booking
-from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer
+from .serializers import FlightSerializer, BookingSerializer, BookingDetailsSerializer, UpdateBookingSerializer, RegisterUserSerializer
 
+class RegisterUser(CreateAPIView):
+	serializer_class = RegisterUserSerializer
 
 class FlightsList(ListAPIView):
 	queryset = Flight.objects.all()
@@ -40,4 +42,3 @@ class BookFlight(CreateAPIView):
 
 	def perform_create(self, serializer):
 		serializer.save(user=self.request.user, flight_id=self.kwargs['flight_id'])
-
